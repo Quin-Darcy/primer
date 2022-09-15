bcba89f5438960204bb65f12b5c3203ca7cce45d
Nimrafets Lamensta <qdarcy@csus.edu>
2022-09-14 21:01:49 -0700

    added 1 bits to assure odd and of needed bit size

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,12 @@ pub fn get_rand_nbit(num_of_bits: u32) -> BigUint {
     let mut rng = rand::thread_rng();
     let mut rand_bits: Vec<u8> = Vec::new();
     
-    for _ in 0..(num_of_bits as usize) {
+    rand_bits.push(1_u8);
+    for _ in 0..(num_of_bits-2 as usize) {
         rand_bits.push(rng.gen_range(0..2) as u8);
     }
+    rand_bits.push(1_u8);
+
     BigUint::from_radix_be(&rand_bits[..], 2).unwrap()
 }
 
