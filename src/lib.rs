ccc6fa7b28f1c90803b9e4a9d1ae9952edd93fac
Quin Darcy <quintondarcy@csus.edu>
2022-09-18 19:47:19 -0700

    API now returns u128

--- src/lib.rs
+++ src/lib.rs
@@ -100,13 +100,16 @@ fn miller_rabin(candidate: BigUint) -> bool {
 }
 
 // This function returns a num_of_bits-bit prime number. 
-pub fn get_large_prime(num_of_bits: u32) -> BigUint {
+pub fn get_large_prime(num_of_bits: u32) -> u128 {
+    if num_of_bits > 128 {
+      num_of_bits = 128_u32;
+    }
     let mut candidate = get_rand_nbit(&num_of_bits);
 
     loop {
         if initial_div_test(candidate.clone()) {
             if miller_rabin(candidate.clone()) {
-                return candidate;
+                return candidate.to_u128().unwrap();
             } else {
                 candidate = get_rand_nbit(&num_of_bits);
             }
